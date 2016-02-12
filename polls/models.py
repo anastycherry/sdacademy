d3bab42f78316bcf9a7d5a4c4d995408d123b8f4
Anasty <anastasii.chernaya@oracle.com>
2016-02-12 18:16:36 +0200

    81

--- polls/models.py
+++ polls/models.py
@@ -7,7 +7,8 @@ from django.utils import timezone
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
-    def __str__(self):            
+    
+    def __unicode__(self):            
         return self.question_text
     
     def was_published_recently(self):
@@ -20,6 +21,7 @@ class Choice(models.Model):
     question = models.ForeignKey(Question)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
-    def __str__(self):              
+    
+    def __unicode__(self):              
         return self.choice_text
 
