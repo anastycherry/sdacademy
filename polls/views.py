94413a930a1ea598999c235b6e2ae13c4481a3a0
Anasty <anastasii.chernaya@oracle.com>
2016-02-13 13:43:55 +0200

    82

--- polls/views.py
+++ polls/views.py
@@ -19,7 +19,6 @@ class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
 
-
 class ResultsView(generic.DetailView):
     model = Question
     template_name = 'polls/results.html'
